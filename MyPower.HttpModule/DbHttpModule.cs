d20215fd52b8d3fb08b6d2d5be7816913e54ffbd
daidch <daidch2011@126.com>
2016-01-20 14:58:28 +0800

    添加 HttpModule

---
+++ MyPower.HttpModule/DbHttpModule.cs
@@ -0,0 +1,41 @@
+﻿using MyPower.Factory;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MyPower.HttpModule
+{
+    public class DbHttpModule : IHttpModule
+    {
+        #region IHttpModule 成员
+
+        public void Dispose()
+        {
+
+        }
+
+        public void Init(HttpApplication context)
+        {
+            context.BeginRequest += new EventHandler(context_BeginRequest);
+            context.EndRequest += new EventHandler(context_EndRequest);
+        }
+
+        void context_BeginRequest(object sender, EventArgs e)
+        {
+            //DBFactory df = new DBFactory();
+            //HttpContext.Current.Session[DBFactory.MyPowerConStrName] = df;
+            //df.CreateMyPowerConStr();
+        }
+
+        void context_EndRequest(object sender, EventArgs e)
+        {
+            //DBFactory df = HttpContext.Current.Session[DBFactory.MyPowerConStrName] as DBFactory;
+            //if (df != null)
+            //{
+            //    df.CreateMyPowerConStr();
+            //}
+        }
+        #endregion
+    }
+}
